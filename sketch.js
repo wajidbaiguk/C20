8670802683df1f1aebb3b29571ec930d20c62132
wajidbaiguk <110336747+wajidbaiguk@users.noreply.github.com>
2022-10-20 12:43:07 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,51 +1,142 @@
-
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
 
-let engine;
-let world;
-var ball;
-var ground;
+var engine, world;
 
-function setup() {
-  createCanvas(400,400);
+var particle1, particle2,particle3,particle4,particle5;
+var particle6, particle7,particle8,particle9,particle10;
+var rotator1, rotator2, rotator3;
+var block1, block2;
 
-  engine = Engine.create();
-  world = engine.world;
-  
-   var ball_options = {
-    restitution: 0.95,
-    frictionAir:0.01
-  }
-   
-   var ground_options ={
-     isStatic: true
-   };
-  
+var angle1=60;
+var angle2=60;
+var angle3=60;
 
-  ground = Bodies.rectangle(200,390,400,20,ground_options);
-  World.add(world,ground);
-ground2=Bodies.rectangle(300,200,200,20,ground_options)
-World.add(world,ground2)
-  ball = Bodies.circle(100,10,20,ball_options);
-  World.add(world,ball);
-  ball2=Bodies.circle(250,10,30,{mass:2,restitution: 0.8,
-    frictionAir:0.05})
-  World.add(world,ball2)
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
+function setup(){
+    var canvas = createCanvas(550,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    //created plane and block bodies
+    var plane_options={
+      isStatic: true
+    }
+
+    plane = Bodies.rectangle(600,height,1200,20,plane_options);
+    World.add(world,plane);
+    block1=Bodies.rectangle(100,400,150,20,plane_options);
+    World.add(world,block1);
+    block2=Bodies.rectangle(400,400,150,20,plane_options);
+    World.add(world,block2);
+
+    //created multiple of particle bodies 
+    var particle_options = {
+      restitution:0.9,
+      friction:0.03
+    }
+
+    particle1 = Bodies.circle(220,10,10,particle_options);
+    World.add(world,particle1);
+
+    particle2 = Bodies.circle(220,10,10,particle_options);
+    World.add(world,particle2);
+
+    particle3 = Bodies.circle(225,10,10,particle_options);
+    World.add(world,particle3);
+
+    particle4 = Bodies.circle(230,10,10,particle_options);
+    World.add(world,particle4);
+
+    particle5 =Bodies.circle(230,10,10,particle_options);
+    World.add(world,particle5);
+
+    particle6 =Bodies.circle(235,10,10,particle_options);
+    World.add(world,particle6);
+
+    particle7 =Bodies.circle(240,10,10,particle_options);
+    World.add(world,particle7);
+
+    particle8 =Bodies.circle(260,10,10,particle_options);
+    World.add(world,particle8);
+
+    particle9 =Bodies.circle(246,10,10,particle_options);
+    World.add(world,particle9);
+
+    particle10 =Bodies.circle(254,10,10,particle_options);
+    World.add(world,particle10);
+
+    var rotator_options={
+      // isStatic=true
+      // isStatic true
+      // isStatic:false
+      isStatic:true
+    };
+
+    // rotator1 = rectangle(250,200,150,20,rotator_options);
+    // World.add(world,rotator1);
+
+    // rotator1 = Bodies.rectangle();
+    // World.add(world,rotator1);
+
+    // rotator1 = Bodies.circle(250,200,150,20,rotator_options);
+    // World.add(world,rotator1);
+
+    rotator1 = Bodies.rectangle(250,200,150,20,rotator_options);
+    World.add(world,rotator1);
+
+    rotator2 = Bodies.rectangle(250,200,150,20,rotator_options);
+    World.add(world,rotator2);
+
+    rotator3 = Bodies.rectangle(250,200,150,20,rotator_options);
+    World.add(world,rotator3);
+
+    //styling the bodies here
+    fill("red");
+    rectMode(CENTER);
+    ellipseMode(RADIUS);
+
 }
 
+function draw(){
+    background("lightgreen");
+    Engine.update(engine);
 
-function draw() 
-{
-  background(51);
-  Engine.update(engine);
-  ellipse(ball2.position.x,ball2.position.y,30);
-  ellipse(ball.position.x,ball.position.y,20);
-  rect(ground.position.x,ground.position.y,400,20);
-  rect(ground2.position.x,ground2.position.y,200,20);
+  //created shape for plane and stand
+  rect(plane.position.x,plane.position.y,1200,20);
+  rect(block1.position.x,block1.position.y,150,20);
+  rect(block2.position.x,block2.position.y,150,20);
+
+  //created shape for all the paticles
+  ellipse(particle1.position.x,particle1.position.y,10);
+  ellipse(particle2.position.x,particle2.position.y,10);
+  ellipse(particle3.position.x,particle3.position.y,10);
+  ellipse(particle4.position.x,particle4.position.y,10);
+  ellipse(particle5.position.x,particle5.position.y,10);
+
+  //created shape for all the rotators
+  Matter.Body.rotate(rotator1,angle1)
+  push();
+  translate(rotator1.position.x,rotator1.position.y);
+  rotate(angle1);
+  rect(0,0,150,20);
+  pop();
+  angle1 +=0.2;
+
+  Matter.Body.rotate(rotator2,angle2)
+  push();
+  translate(rotator2.position.x,rotator2.position.y);
+  rotate(angle2);
+  rect(0,0,150,20);
+  pop();
+  angle2 +=0.3;
+
+  Matter.Body.rotate(rotator3,angle3)
+  push();
+  translate(rotator3.position.x,rotator3.position.y);
+  rotate(angle3);
+  rect(0,0,150,20);
+  pop();
+  angle3 +=0.4;
+    
 }
-
